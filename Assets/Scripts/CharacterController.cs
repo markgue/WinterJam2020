ce49ed320a3d3fddcb01540ee36d4270ee789cc5
cuoshen <chairmantoad@gmail.com>
2020-12-21 05:38:40 +0800

    Create base class for interactables

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,25 +4,5 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    #region Movement
-    // TODO: Simple vector based movement
-    // TODO: Integrate animation
-    #endregion
-
-    #region Interaction
-    // TODO: Abstract interaction
-    // TODO: Integrate animation
-    #endregion
 }
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class Interactable
+{
+    public abstract void Pickup();
+
+    public abstract void PutDown();
+}
