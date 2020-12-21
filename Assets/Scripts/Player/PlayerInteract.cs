e33cb8782b427c0c0d3ae5487b274721220efc73
Xiangyu Wan <johnnywon@g.ucla.edu>
2020-12-20 22:44:51 -0800

    made a dummy interaction controller

---
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    /// <summary>
+    /// what do we put here?
+    ///     player needs to interact with materials, tools (axe) and machines
+    /// </summary>
+
+    // key settings
+    public KeyCode interactKey = KeyCode.E;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
