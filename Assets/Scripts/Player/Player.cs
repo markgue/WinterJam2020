a9782450c53e150fe6c2f291c04ced44be3dc012
Xiangyu Wan <johnnywon@g.ucla.edu>
2020-12-22 12:15:26 -0800

    added animator parameters to player script

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -42,7 +42,6 @@ class Player : MonoBehaviour {
 
     void Update() 
     {
-        // this game won't need ground checking.
         grounded = Physics.CheckSphere(groundChecker.position, groundBuffer, groundMask);
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -50,27 +49,33 @@ class Player : MonoBehaviour {
         Vector3 move = new Vector3(x, 0, z);
         move *= speed * Time.deltaTime;
 
+        // rotate the player and check running animation
         if (move != Vector3.zero) {
             Quaternion rotation = Quaternion.LookRotation(move, Vector3.up);
             transform.rotation = rotation;
+            gameObject.GetComponent<Animator>().SetBool("isRunning", true);
+        }
+        else {
+            gameObject.GetComponent<Animator>().SetBool("isRunning", false);
         }
 
         controller.Move(move);
 
-        gVelocity.y -= g * Time.deltaTime;
-
-        if (grounded && gVelocity.y <= 0)
-        {
-            gVelocity.y = -0.1f;
-        }
-
+        // ground check and pushing player onto ground
+        //gVelocity.y -= g * Time.deltaTime;
+        //if (grounded && gVelocity.y <= 0)
+        //{
+            gVelocity.y = (grounded && gVelocity.y <= 0)? - 0.1f : g * Time.deltaTime;
+        //}
         controller.Move(gVelocity * Time.deltaTime);
 
+        // tell items to show hover menu (not menu really but seems hover menu sounds more smooth :)
         Collider[] hitItems = Physics.OverlapSphere(grabPoint.position, grabRadius, itemMask);
         foreach (Collider it in hitItems) {
             it.gameObject.GetComponent<Item>().Hover();
         }
 
+        // pickup / throw
         if (Input.GetKeyDown(PickAndThrowKey) || Input.GetKeyDown(PickAndThrowJoystick))
         {
             if (ItemHolding != null) {
@@ -82,9 +87,11 @@ class Player : MonoBehaviour {
                 ItemHolding.gameObject.GetComponent<Item>().Put();
                 ItemHolding = null;
                 Debug.Log("item thrown");
+                gameObject.GetComponent<Animator>().SetTrigger("Throw");
             }
             else {
                 if (hitItems.Length > 0) {
+                    // pick the found item
                     Item target = hitItems[0].gameObject.GetComponent<Item>();
                     if (target != null) {
                         if (Vector3.Distance(target.transform.position, transform.position) <= grabbingRange) {
@@ -95,6 +102,7 @@ class Player : MonoBehaviour {
                             target.Take();
                             Debug.Log("Item picked up");
                         }
+                        gameObject.GetComponent<Animator>().SetTrigger("Pickup");
                     }
                 }
             }
@@ -132,6 +140,14 @@ class Player : MonoBehaviour {
             //        Debug.Log("item thrown");
             //    }
             //}
+        }
+
+        // checking holding animation
+        if (ItemHolding != null) {
+            gameObject.GetComponent<Animator>().SetBool("isHolding", true);
+        }
+        else {
+            gameObject.GetComponent<Animator>().SetBool("isHolding", false);
         }
     }
 }
