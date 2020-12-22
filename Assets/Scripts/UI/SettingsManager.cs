220f1a78e7493aa663ab33dc28a69dc273a26a34
Xiangyu Wan <johnnywon@g.ucla.edu>
2020-12-22 12:58:47 -0800

    let's use SettingsManager

---
+++ Assets/Scripts/UI/SettingsManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    // singleton /////////////////////////////////////
+    public static SettingsManager instance = null;
+
+
+    // setting parameters ////////////////////////////
+    public float volume;
+
+
+    // system messages ///////////////////////////////
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
